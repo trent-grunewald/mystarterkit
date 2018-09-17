cfb82083ae5e473ada24f3efaae37fac9392a3fe
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-09-17 11:39:56 -0700

    roots

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,47 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const babel = require("babel");
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const shell = require('gulp-shell');
 
-gulp.task('sass', function (){
+
+gulp.task('sass', () =>{
+  //targets any folder inside the src/scss that has scss files
   //targets any folder inside the src/scss that has scss files
   return gulp.src('./src/scss/**/*.scss')
   //displays any error in console log
   .pipe(sass().on('error', sass.logError))
-
   //automatically adds the autoprefixer to converted css
   .pipe(autoprefixer({
     browsers: ['last 2 versions']
   }))
-
-  //converts all the folder files to css
-  .pipe(gulp.dest('./public/css'));
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('./public/css'))
 });
+
+gulp.task('webpack', () => {
+  return gulp.src('*.js', {read: false})
+  .pipe(shell([
+    'webpack'
+  ]))
+})
+
+gulp.task('sass:minify', () => {
+  //targets any folder inside the src/scss that has scss files
+  return gulp.src('./public/css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./public/css'));
+  });
 
 
 //defaults to first task (sass) and adds a function
-gulp.task('default', ['sass'], function(){
+gulp.task('default', ['sass', 'webpack'], () => {
   //watchest the SCSS folder for any changes and runs the 'sass' task on any changes-- resulting in automatic css conversion.
   gulp.watch('./src/scss/**/*', ['sass'])
+  gulp.watch('./src/js/**/*', ['webpack'])
 });
+
+gulp.task('production', ['sass:minify'])
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+// const babel = require("babel-core");
 
 module.exports = {
   entry: {
+    //YOUR hand typed JS "app" will be the output name
     app: './src/js/firstComp/index.js',
     //vendor = js libraries
     vendor: ['react']
   },
+  //Converted JS location [name].js uses the entry name "app" in this case (see above).
   output: { filename: '[name].js',
             path: path.resolve(__dirname,
               'public/js/componenets')},
@@ -14,9 +17,13 @@ module.exports = {
   module: {
     rules: [
       {
+        //looks at any file that ends with .js
         test: /\.js$/,
+        //exclude files from node_modules
         exclude: /node_modules/,
+        //use the babel loader for conversion
         loader: 'babel-loader',
+        //conversion presets (es2015)
         options: {
           presets: [
             [ 'es2015', { modules: false} ]
@@ -24,16 +31,5 @@ module.exports = {
         }
       }
     ]
-  },
-
-  plugins: [
-    //
-          new webpack.optimize.CommonsChunkPlugin({
-              name: 'vendor',
-              minChunks: function (module) {
-                return module.context &&
-                module.context.indexOf('node_modles') !== -1;
-              }
-          }),
-  ]
+  }
 }
